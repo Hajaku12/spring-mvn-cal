cfca1c542d2963fced80461a6e7a28abb494d594
unknown <hann.jang@labinfo.is.escuelaing.edu.co>
2024-06-25 12:53:18 -0500

    Actualizado

---
+++ src/main/java/edu/escuelaing/arsw/GreetingController.java
@@ -0,0 +1,18 @@
+package edu.escuelaing.arsw;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+//Se hace un singleton por tener solo una instancia
+public class GreetingController {
+
+    @GetMapping("/greeting") //Responder con GET
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
